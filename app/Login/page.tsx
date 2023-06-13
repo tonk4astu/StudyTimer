6ea43a48798b0eb2db017a840831006c6cbb0211
tonk4atu <norimaki229@gmail.com>
2023-06-13 15:56:09 +0900

    認証機能

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -5,7 +5,6 @@ import EmailAuth from "../Components/EmailAuth";
 export default async function Login() {
     const [mail,setMail] = useState("");
     const [password,setPassword] = useState("");
-    EmailAuth({email:mail,password:password,types:'signIn'});
     return (
         <div>
                 <input type="text" value={mail} onChange={(event)=>setMail(event.target.value)}></input>
