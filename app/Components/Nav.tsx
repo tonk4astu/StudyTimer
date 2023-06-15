02e5b2e1d15abeb41b5c9505a5efdf2b9e024a93
tonk4atu <norimaki229@gmail.com>
2023-06-15 18:07:40 +0900

    Errorが出ます

--- app/Components/Nav.tsx
+++ app/Components/Nav.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 export default function Nav() {
   return (
     <nav className="flex flex-row justify-between items-center">
@@ -6,9 +7,11 @@ export default function Nav() {
             <h1 className="text-2xl font-bold">Study Timer</h1>
         </div>
         <div className="flex flex-row items-center">
+            <Link href="/Login">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Sign In
             </button>
+            </Link>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Sign Up
             </button>
--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -2,13 +2,27 @@
 import { useState } from "react";
 import EmailAuth from "../Components/EmailAuth";
 
+//ログイン機能
 export default async function Login() {
-    const [mail,setMail] = useState("");
+    const [mail,setMail] = useState<string>("");
     const [password,setPassword] = useState("");
     return (
         <div>
-                <input type="text" value={mail} onChange={(event)=>setMail(event.target.value)}></input>
-                <input type="text" value={password} onChange={(event)=>setPassword(event.target.value)}></input>
+                Mail:
+            <input 
+            className=" mr-5 text-black font-bold py-0 px-4 rounded border-2 border-black" 
+            type="text"
+            value={mail} 
+            onChange={()=>setMail(mail)}
+            />
+                Password:
+            <input 
+            className=" mr-5 text-black font-bold py-0 px-4 rounded border-2 border-black" 
+            type="text"
+            value={password} 
+            onChange={(e)=>setPassword(e.target.value)}
+            />
         </div>
     )
 }
+
