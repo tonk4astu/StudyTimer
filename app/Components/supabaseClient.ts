4e1f27f83548d653fab384ea2ed42f22cac937b9
tonk4atu <norimaki229@gmail.com>
2023-09-28 15:22:44 +0900

    refreshTokenのエラー修正 これで解決 https://github.com/supabase/supabase/issues/14234

--- app/Components/supabaseClient.ts
+++ app/Components/supabaseClient.ts
@@ -3,4 +3,4 @@ import {createClient} from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
-export const supabase = createClient(supabaseUrl, supabaseKey,{ auth: {persistSession: false } });
+export const supabase = createClient(supabaseUrl, supabaseKey,{ auth: {persistSession: false,autoRefreshToken: false } });
