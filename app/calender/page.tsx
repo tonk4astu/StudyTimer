fd65abe7aeae2c50b811065e93f80723a1921a2b
tonk4atu <norimaki229@gmail.com>
2023-10-19 19:56:30 +0900

    カレンダーの枠組みを追加

--- app/calender/page.tsx
+++ app/calender/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+import "@/globals.css"
 import { supabase } from "@/Components/supabaseClient"
 import { UserResponse } from "@supabase/supabase-js"
 import Nav from "@/Components/Layout/nav"
@@ -8,7 +10,6 @@ type Calender = {
 
 const getUserData = async (month: string) => {
   const { data } = await supabase.auth.getUser()
-  console.dir(await data)
   //TODO:supabaseからユーザーの今月の実績データを取得する
 }
 
@@ -28,27 +29,45 @@ const calenderPages = async () => {
 
 export default calenderPages
 
+//カレンダーのレイアウトを作成する
 function CalenderLayout(props: Calender) {
   const defaultProps = {
-    days: 30,
+    days: 32,
     children: "test",
   }
   const dayBox = (index: number) => {
-    return <div key={index} className={"border border-slate-900"}></div>
+    return (
+      <div
+        key={index}
+        className={
+          "border border-slate-900 hover:bg-slate-400 container mx-auto"
+        }
+      >
+        <button className={"w-full h-full"} onClick={(e) => StudyScoreView(e)}>
+          {index}
+        </button>
+      </div>
+    )
   }
 
   const layout = () => {
     const dayBoxes = []
-    for (let i = 0; i < props.days; i++) {
+    for (let i = 1; i < props.days; i++) {
       dayBoxes.push(dayBox(i))
     }
     return dayBoxes
   }
   return (
-    <div className={"grid grid-rows-6 grid-cols-7"}>
+    <div className={"grid grid-rows-6 grid-cols-7 min-h-screen"}>
       {layout().map((day) => {
         return day
       })}
     </div>
   )
+}
+
+const StudyScoreView = (event: React.MouseEvent<HTMLButtonElement>) => {
+  event.preventDefault()
+  console.log("test")
+  //TODO:supabaseからユーザーの当日の実績データを取得する
 }
